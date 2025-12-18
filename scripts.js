0e322b4b0991e652a9fb203e46d56f8986dd48fe
ChaneyWilliams <chaneyw2004@gmail.com>
2025-12-18 14:47:12 -0600

    Organizing

---
+++ scripts.js
@@ -0,0 +1,10 @@
+class Project {
+    constructor(title, description, timeFrame = "NA", imageURL) {
+        this.title = title;
+        this.description = description;
+        this.timeFrame = timeFrame;
+        this.image = imageURL;
+    }
+}
+
+//const SharkTrivia = Project("Shark Trivia", )
